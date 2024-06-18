cf91c2f3a8662f86727938cbebaed9e5c6052933
Eosgamez <gilgoona@gmail.com>
2024-06-18 12:59:49 +0900

    [Ollama] insert provider
    insert provider

---
+++ forge/forge/llm/providers/ollama.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+import enum
+import logging
+from typing import Any, Optional
+
+import tiktoken
+from pydantic import SecretStr
+
+from forge.models.config import UserConfigurable
+
+from ._openai_base import BaseOpenAIChatProvider
+from .schema import (
+    ChatModelInfo,
+    ModelProviderBudget,
+    ModelProviderConfiguration,
+    ModelProviderCredentials,
+    ModelProviderName,
+    ModelProviderSettings,
+    ModelTokenizer,
+)
+
+class OllamaModelName(str, enum.Enum):
+    LLAMA3_8B = "llama3-8b-ollama"
+    LLAMA3_70B = "llama3-70b-ollama"
+
+OLLAMA_CHAT_MODELS = {
+    info.name: info
+    for info in [
+        ChatModelInfo(
+            name=OllamaModelName.LLAMA3_8B,
+            provider_name=ModelProviderName.OLLAMA,
+            prompt_token_cost=0.05 / 1e6,
+            completion_token_cost=0.10 / 1e6,
+            max_tokens=8192,
+            has_function_call_api=True,
+        ),
+        ChatModelInfo(
+            name=OllamaModelName.LLAMA3_70B,
+            provider_name=ModelProviderName.OLLAMA,
+            prompt_token_cost=0.59 / 1e6,
+            completion_token_cost=0.79 / 1e6,
+            max_tokens=8192,
+            has_function_call_api=True,
+        ),
+    ]
+}
+
+class OllamaCredentials(ModelProviderCredentials):
+    """Credentials for Ollama."""
+    api_base: Optional[SecretStr] = UserConfigurable(
+        default=None, from_env="OLLAMA_API_BASE_URL"
+    )
+
+    def get_api_access_kwargs(self) -> dict[str, str]:
+        return {
+            k: v.get_secret_value()
+            for k, v in {
+                "base_url": self.api_base,
+            }.items()
+            if v is not None
+        }
+
+
+class OllamaSettings(ModelProviderSettings):
+    credentials: Optional[OllamaCredentials]  # type: ignore
+    budget: ModelProviderBudget  # type: ignore
+
+class OllamaProvider(BaseOpenAIChatProvider[OllamaModelName, OllamaSettings]):
+    CHAT_MODELS = OLLAMA_CHAT_MODELS
+    MODELS = CHAT_MODELS
+
+    default_settings = OllamaSettings(
+        name="groq_provider",
+        description="Provides access to Groq's API.",
+        configuration=ModelProviderConfiguration(),
+        credentials=None,
+        budget=ModelProviderBudget(),
+    )
+
+    _settings: OllamaSettings
+    _configuration: ModelProviderConfiguration
+    _credentials: OllamaCredentials
+    _budget: ModelProviderBudget
+
+    def __init__(
+        self,
+        settings: Optional[OllamaSettings] = None,
+        logger: Optional[logging.Logger] = None,
+    ):
+        super(OllamaProvider, self).__init__(settings=settings, logger=logger)
+
+        from groq import AsyncGroq
+
+        self._client = AsyncGroq(
+            **self._credentials.get_api_access_kwargs()  # type: ignore
+        )
+
+    def get_tokenizer(self, model_name: OllamaModelName) -> ModelTokenizer[Any]:
+        # HACK: No official tokenizer is available for Groq
+        return tiktoken.encoding_for_model("gpt-3.5-turbo")
